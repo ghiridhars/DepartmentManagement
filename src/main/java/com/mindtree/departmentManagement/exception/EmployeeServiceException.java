d4541ec9d8572d0758c28352bb7bdd9ff9c0a951
Ghiridhar <ghiridhars@gmail.com>
2021-03-30 20:41:34 +0530

    Second

---
+++ src/main/java/com/mindtree/departmentManagement/exception/EmployeeServiceException.java
@@ -0,0 +1,31 @@
+package com.mindtree.departmentManagement.exception;
+
+public class EmployeeServiceException extends DepartmentManageException {
+
+	public EmployeeServiceException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmployeeServiceException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmployeeServiceException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmployeeServiceException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmployeeServiceException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
